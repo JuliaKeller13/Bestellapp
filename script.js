de400d1a68116b487719bdc9d003acd1ec3e775b
Julia Keller <juliakeller.vk@gmail.com>
2026-01-24 11:53:04 +0100

    basket rendern and price calculate

--- script.js
+++ script.js
@@ -1,48 +1,62 @@
 const basket = [];
 
 function init() {
-    renderFoodCards();
-    renderBasket();
+  renderFoodCards();
+  renderBasket();
 }
 
 function renderFoodCards() {
-    const menuCardsCakesSection = document.getElementById("menuCardsCakesSection");
-    const menuCardsCheescakesSection = document.getElementById("menuCardsCheescakesSection");
-    const menuCardsCupcakesSection = document.getElementById("menuCardsCupcakesSection");
-    const menuCardsDonutsSection = document.getElementById("menuCardsDonutsSection");
-    menuCardsCakesSection.innerHTML = "";
-    menuCardsCheescakesSection.innerHTML = "";
-    menuCardsCupcakesSection.innerHTML = "";
-    menuCardsDonutsSection.innerHTML = "";
+  const menuCardsCakesSection = document.getElementById(
+    "menuCardsCakesSection",
+  );
+  const menuCardsCheescakesSection = document.getElementById(
+    "menuCardsCheescakesSection",
+  );
+  const menuCardsCupcakesSection = document.getElementById(
+    "menuCardsCupcakesSection",
+  );
+  const menuCardsDonutsSection = document.getElementById(
+    "menuCardsDonutsSection",
+  );
+  menuCardsCakesSection.innerHTML = "";
+  menuCardsCheescakesSection.innerHTML = "";
+  menuCardsCupcakesSection.innerHTML = "";
+  menuCardsDonutsSection.innerHTML = "";
 
-    for (let articlesIndex = 0; articlesIndex < articles.length; articlesIndex++) {
-    if (articles[articlesIndex].category === "Kuchen") {
-        menuCardsCakesSection.innerHTML += getFoodCardsHtml(articlesIndex);
-    } if (articles[articlesIndex].category === "Käsekuchen") {
-        menuCardsCheescakesSection.innerHTML += getFoodCardsHtml(articlesIndex);
-    } if (articles[articlesIndex].category === "Donuts") {
-        menuCardsDonutsSection.innerHTML += getFoodCardsHtml(articlesIndex);
-    } if (articles[articlesIndex].category === "Muffins") {
-        menuCardsCupcakesSection.innerHTML += getFoodCardsHtml(articlesIndex);
+  for (
+    let articleIndex = 0;
+    articleIndex < articles.length;
+    articleIndex++
+  ) {
+    if (articles[articleIndex].category === "Kuchen") {
+      menuCardsCakesSection.innerHTML += getFoodCardsHtml(articleIndex);
     }
-}
+    if (articles[articleIndex].category === "Käsekuchen") {
+      menuCardsCheescakesSection.innerHTML += getFoodCardsHtml(articleIndex);
+    }
+    if (articles[articleIndex].category === "Donuts") {
+      menuCardsDonutsSection.innerHTML += getFoodCardsHtml(articleIndex);
+    }
+    if (articles[articleIndex].category === "Muffins") {
+      menuCardsCupcakesSection.innerHTML += getFoodCardsHtml(articleIndex);
+    }
+  }
 }
 
 //ad to basket
-function addToBasket(articlesIndex) {
-    const article = articles[articlesIndex];
+function addToBasket(articleIndex) {
+    const article = articles[articleIndex];
     
-    let existingArticles = null;
-    for (let articlesIndex = 0; articlesIndex < basket.length; articlesIndex++) {
-        if (basket[articlesIndex].title === article.title) {
-            existingArticles = basket[articlesIndex];
-            return;
+    let existingArticle = null;
+    for (let basketArticleIndex = 0; basketArticleIndex < basket.length; basketArticleIndex++) {
+        if (basket[basketArticleIndex].title === article.title) {
+            existingArticle = basket[basketArticleIndex];
+            break;
         }
     }
 
-    if (existingArticles) {
-        existingArticles.amount++;
-
+    if (existingArticle) {
+        existingArticle.amount++;
     } else {
         basket.push({
             "title": article.title,
@@ -53,3 +67,53 @@ function addToBasket(articlesIndex) {
     
     renderBasket();
 }
+
+function renderBasket() {
+  let totalSum = 0;
+  const basketContent = document.getElementById("basketContent");
+  basketContent.innerHTML = "";
+
+  if (basket.length === 0) {
+    basketContent.innerHTML = `<div class="basket-empty">
+                <div>
+                  Füge einige leckere Gerichte aus der Speisekarte hinzu
+                </div>
+                <img
+                  class="basket-empty-img"
+                  src="./assets/icons/shopping-cart-basket.png"
+                  alt="leerer Einkaufswagen"
+                />
+              </div>`;
+    return;
+  }
+
+  for (let articleIndex = 0; articleIndex < basket.length; articleIndex++) {
+    const article = basket[articleIndex];
+    const subtotal = article.price * article.amount;
+    totalSum += subtotal;
+
+    basketContent.innerHTML += `
+            <div class="basket-articles">
+                <div class="basket-article-info">
+                    <b>${article.title}</b><br>
+                    ${article.price.toFixed(2)}€ x ${article.amount} = ${subtotal.toFixed(2)}€
+                </div>
+                <div class="basket-item-controls">
+                    <button onclick="changeAmount(${articleIndex}, -1)">-</button>
+                    <span>${article.amount}</span>
+                    <button onclick="changeAmount(${articleIndex}, 1)">+</button>
+                </div>
+            </div>
+            <hr>
+        `;
+  }
+
+  basketContent.innerHTML += `
+        <div class="basket-total">
+            <strong>Gesamtsumme: ${totalSum.toFixed(2)}€</strong>
+        </div>
+    `;
+}
+
+
+//im Warenkorb Menge soll veränderbar sein, oder löschen bei 1
--- scripts/templates.js
+++ scripts/templates.js
@@ -1,15 +1,15 @@
-function getFoodCardsHtml(articlesIndex) {
+function getFoodCardsHtml(articleIndex) {
     return `
         <div class="food-card">
-            <img class="food-card-img" src="${articles[articlesIndex].img}" alt="${articles[articlesIndex].title}">
+            <img class="food-card-img" src="${articles[articleIndex].img}" alt="${articles[articleIndex].title}">
             <div class="food-card-info">
                 <div class="food-card-title-with-description">
-                    <h3>${articles[articlesIndex].title}</h3>
-                    <p class="description">${articles[articlesIndex].description}</p>
+                    <h3>${articles[articleIndex].title}</h3>
+                    <p class="description">${articles[articleIndex].description}</p>
                 </div>
                 <div class="food-card-buttons">
-                    <p>${articles[articlesIndex].price.toFixed(2)}€</p>
-                    <button class="add-to-basket-button" onclick="addToBasket(${articlesIndex})">Hinzufügen</button>
+                    <p>${articles[articleIndex].price.toFixed(2)}€</p>
+                    <button class="add-to-basket-button" onclick="addToBasket(${articleIndex})">Hinzufügen</button>
                 </div>
             </div>
         </div>
